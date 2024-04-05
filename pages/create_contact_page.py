56245ee5dee5620d207ec63900a6085eeac61cdf
Lipatnikova <aerovaya@gmail.com>
2024-04-05 23:38:10 +0400

    feat: history and test steps allure

--- pages/create_contact_page.py
+++ pages/create_contact_page.py
@@ -1,3 +1,5 @@
+import allure
+
 from base.base_page import BasePage
 from generator.generator import create_contact
 from locators.create_contact_locators import CreateContactLocators as Locator
@@ -5,35 +7,41 @@ from locators.create_contact_locators import CreateContactLocators as Locator
 
 class CreateContactPage(BasePage):
 
-    def enter_first_name(self):
-        info = next(create_contact())
-        first_name = info.first_name
-        self.fill_in_input(Locator.FIRST_NAME, first_name)
-        return first_name
+    def enter_first_name(self) -> str:
+        with allure.step('Заполнить поле First name валидными данными'):
+            info = next(create_contact())
+            first_name = info.first_name
+            self.fill_in_input(Locator.FIRST_NAME, first_name)
+            return first_name
 
-    def enter_last_name(self):
-        info = next(create_contact())
-        last_name = info.last_name
-        self.fill_in_input(Locator.LAST_NAME, last_name)
-        return last_name
+    def enter_last_name(self) -> str:
+        with allure.step('Заполнить поле Last name валидными данными'):
+            info = next(create_contact())
+            last_name = info.last_name
+            self.fill_in_input(Locator.LAST_NAME, last_name)
+            return last_name
 
-    def enter_title(self):
-        info = next(create_contact())
-        title = info.title
-        self.fill_in_input(Locator.TITLE, title)
-        return title
+    def enter_title(self) -> str:
+        with allure.step('Заполнить поле Title валидными данными'):
+            info = next(create_contact())
+            title = info.title
+            self.fill_in_input(Locator.TITLE, title)
+            return title
 
-    def enter_email(self):
-        info = next(create_contact())
-        email = info.email
-        self.fill_in_input(Locator.EMAIL, email)
-        return email
+    def enter_email(self) -> str:
+        with allure.step('Заполнить поле Email валидными данными'):
+            info = next(create_contact())
+            email = info.email
+            self.fill_in_input(Locator.EMAIL, email)
+            return email
 
-    def choose_account_manager(self):
-        name_manager = 'Jane Doe'
-        self.click_element(Locator.ACCOUNT_MANAGER)
-        self.click_element(Locator.MANAGER_NAME)
-        return name_manager
+    def choose_account_manager(self) -> str:
+        with allure.step('Выбрать аккаунт менеджера из выпадающего списка'):
+            name_manager = 'Jane Doe'
+            self.click_element(Locator.ACCOUNT_MANAGER)
+            self.click_element(Locator.MANAGER_NAME)
+            return name_manager
 
-    def click_btn_save(self):
-        self.click_element(Locator.BTN_SAVE)
+    def click_btn_save(self) -> None:
+        with allure.step('Нажать на кнопку SAVE'):
+            self.click_element(Locator.BTN_SAVE)
--- pages/show_contact_page.py
+++ pages/show_contact_page.py
@@ -1,10 +1,14 @@
+import allure
+
 from base.base_page import BasePage
 from locators.show_contact_locators import ShowContactLocators as Locator
 
 
 class ShowContact(BasePage):
-    def extract_full_name(self):
-        return self.get_text(Locator.FULL_NAME)
+    def extract_full_name_contact(self):
+        with allure.step('Получить полное имя контакта'):
+            return self.get_text(Locator.FULL_NAME)
 
-    def extract_title(self):
-        return self.get_text(Locator.TITLE)[:-3]
+    def extract_title_contact(self):
+        with allure.step('Получить title контакта'):
+            return self.get_text(Locator.TITLE)[:-3]
